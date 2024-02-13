179aa3f5e235a5f31c8442fac8ad4b8aeb9b4848
okifxon <shokirjonovokifxon46@gmail.com>
2024-02-13 23:15:54 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(name,yili,avtor,vaqt,id){
+        this.name = name,
+        this.yili = yili,
+        this.avtor = avtor,
+        this.vaqt = vaqt,
+        this.id = id
+    }
+}
+let musiqa = new User("daydib", 2024, "xamdam-sobirov", 3.30, 18237);
+let musiqa2 = new User("lola", 2015, "ozodbek-nazarbekov", 3.00, 15267);
+let musiqa3 = new User("rayxon", 2024, "ozodbek-nazarbekov", 2.35, 11287);
+let musiqa4 = new User("boyvachcha", 2024, "johongir-poziljonov", 2.10, 24247);
+console.log(musiqa2);
